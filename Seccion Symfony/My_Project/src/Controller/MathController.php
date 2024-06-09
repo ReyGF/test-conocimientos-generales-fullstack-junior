b5f914c407c7c6aeb754e5a1a04237bcc5f5a57f
ReyGF <gomezreynol14@gmail.com>
2024-06-09 14:29:56 -0400

    Update Controllers Response

--- Seccion Symfony/My_Project/src/Controller/MathController.php
+++ Seccion Symfony/My_Project/src/Controller/MathController.php
@@ -11,17 +11,21 @@ use Symfony\Component\Routing\Annotation\Route;
 class MathController extends AbstractController
 {
     private MathService $mathService;
-    
+
     public function __construct(MathService $mathService)
     {
         $this->mathService = $mathService;
     }
 
-    #[Route('/math/sum/{a}/{b}', name : 'math_sum')]
+    #[Route('/math/sum/{a}/{b}', name: 'math_sum')]
     public function sum($a, $b): Response
     {
         $sum = $this->mathService->sum($a, $b);
 
-        return new Response("La suma de $a y $b es $sum.");
+        //return new Response("");
+
+        return $this->render('/math/index.html.twig', [
+            'controller_name' => "La suma de $a y $b es $sum."
+        ]);
     }
 }
--- Seccion Symfony/My_Project/src/Controller/NameController.php
+++ Seccion Symfony/My_Project/src/Controller/NameController.php
@@ -11,6 +11,10 @@ class NameController extends AbstractController
     #[Route('/hello/{name}', name: 'hello')]
     public function index($name = 'World'): Response
     {
-        return new Response('Hello ' . $name . '!');
+        //return new Response('Hello ' . $name . '!');
+
+        return $this->render('/name/index.html.twig', [
+            'controller_name' => $name
+        ]);
     }
 }
